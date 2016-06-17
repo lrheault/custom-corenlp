6c12071e3cdf6c61c157fc09d108ec092b7ef7a6
lrheault <lrheault@users.noreply.github.com>
2016-06-17 00:04:08 -0400

    Update CustomLemmatizer.java

--- CustomLemmatizer.java
+++ CustomLemmatizer.java
@@ -9,28 +9,28 @@ import edu.stanford.nlp.util.*;
 
 public class CustomLemmatizer {
     public static void main (String[] args) throws IOException {
-		// Loading annotators.
+	// Loading annotators.
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
-		props.setProperty("threads", "8");
+	props.setProperty("threads", "8");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-		// Reading in (possibly multi-line) string from standard input (to be called from Python, for instance).		
-		BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+	// Reading in (possibly multi-line) string from standard input (to be called from Python, for instance).		
+	BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
         String line;
-		StringBuilder processedText = new StringBuilder(); 
-		while ( (line = reader.readLine())!= null) {			
-			// Performing annotations.
-			Annotation annotation = new Annotation(line);
-			pipeline.annotate(annotation);
-			// Extracting lemmas.
-			List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-			for(CoreMap sentence: sentences) {    
+	StringBuilder processedText = new StringBuilder(); 
+	while ( (line = reader.readLine())!= null) {			
+		// Performing annotations.
+		Annotation annotation = new Annotation(line);
+		pipeline.annotate(annotation);
+		// Extracting lemmas.
+		List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+		for(CoreMap sentence: sentences) {    
 		    	for(CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {       
-					processedText.append(token.get(CoreAnnotations.LemmaAnnotation.class)).append(" ");
-				}
+				processedText.append(token.get(CoreAnnotations.LemmaAnnotation.class)).append(" ");
 			}
-			processedText.append("\n");
 		}
-		System.out.println(processedText.toString());
+		processedText.append("\n");
+	}
+	System.out.println(processedText.toString());
     }
 }
